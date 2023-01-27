33d6070d3af84e396476f9cf129c0403be82d2c5
mariamdopidze <119893246+mariamdopidze@users.noreply.github.com>
2023-01-27 01:59:19 -0800

    created html js files

---
+++ scrip.js
@@ -0,0 +1,49 @@
+//#2
+function calculateNameLength(currency) {
+    
+
+    if (currency === "USD"){
+        console.log ("$");
+        return "$"
+    }
+    
+     if (currency === "Euro") { console.log("euro");
+       return "euro"} 
+    
+     console.log("ლარი");
+     return"ლ"
+    
+    
+    }
+    
+    
+    calculateNameLength("USD");
+    calculateNameLength("Euro");
+    calculateNameLength("lari");
+    
+    
+    // #6
+    function calcExpense(income, expense) {
+        const profit=income-expense;
+        const calcperc = (profit/ income) * 100;
+        console.log(profit);
+        return profit
+    }
+     const jan = calcExpense(300, 100);
+     const Fab = calcExpense(550, 230);
+     const Mar = calcExpense(870, 320);
+     const sumOfProfit = jan+ Fab + Mar;
+     console.log(sumOfProfit);
+    
+     //#4
+     let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+     const filterEvenNumbers = numbers.filter((number) =>number % 2 ===0);
+     console.log(filterEvenNumbers);
+    
+    // #3
+     let text = "my name is john"
+     let lowerCasetext = text.toLowerCase()
+     let upperCasertext = text. toUpperCase()
+    
+     console.log(upperCasertext)
+     console.log(lowerCasetext)
